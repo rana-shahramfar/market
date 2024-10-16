82dae65a724663da25ec52f71b21fd5fcf6d61f7
rana-shahramfar <rana.shahramfar127@gmail.com>
2024-10-16 16:49:24 +0330

    edit main.js fix time format

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
-var countDownDate = new Date("October 18, 2024 15:30:00").getTime();
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 25, 2024 15:37:25").getTime();
+// Update the count down every 1 second
 var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
-    var minutes = Math.floor(distance % (1000 * 60 * 60)) / (1000 * 60);
-    var seconds = Math.floor(distance % (1000 * 60)) / 1000;
-    document.getElementById("demo").innerHTML = '<span id="day">' + days + '<span id="hours">' + hours + '<span id="minutes">' + minutes + '<span id="minutes">' + seconds + '<span>';
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "به پایان رسید";
-    }
-}, 1000);
+  // Get today's date and time
+  var now = new Date().getTime();
+// Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Output the result in an element with id="demo"
+   document.getElementById("demo").innerHTML =''+ days + '' +  ''+ hours + ''
+   + ''+ minutes + '' + ''+ seconds + '';
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "به پایان رسیده";
+  }
+ }, 1000);
+$(".tt").tooltip("show");
+$(".popovers").popover("show");
+$(".tot").toast("show");
